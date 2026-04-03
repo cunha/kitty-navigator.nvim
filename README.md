fdcaa265d18a236037670d201db37097ccb9afcf
Italo Cunha <cunha@dcc.ufmg.br>
2026-04-03 00:40:43 -0300

    initial import

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # kitty-navigator.nvim
+
 Navigate between Kitty and Neovim panes transparently
+
+This implementation mimics the mode of operation of [knubie/vim-kitty-navigator](https://github.com/knubie/vim-kitty-navigator) in a Lua script based on [MunsMan/kitty-navigator.nvim](https://github.com/MunsMan/kitty-navigator.nvim).
+
+Install with Lazy:
+
+```lua
+
+```
+
+And then update your `.config/kitty/kitty.conf` file to include (adjust the keys according to your preference):
+
+```text
+map kitty_mod+h neighboring_window left
+map kitty_mod+l neighboring_window right
+map kitty_mod+j neighboring_window down
+map kitty_mod+k neighboring_window up
+map --when-focus-on var:ISNVIM kitty_mod+j
+map --when-focus-on var:ISNVIM kitty_mod+k
+map --when-focus-on var:ISNVIM kitty_mod+h
+map --when-focus-on var:ISNVIM kitty_mod+l
+```
+
---
+++ lua/kitty-navigator/init.lua
@@ -0,0 +1,41 @@
+-- adapted from https://github.com/MunsMan/kitty-navigator.nvim
+
+local M = {}
+
+local mappings = { h = "left", j = "bottom", k = "top", l = "right" }
+
+function M.navigate(direction)
+	local left_win = vim.fn.winnr("1" .. direction)
+	if vim.fn.winnr() ~= left_win then
+		vim.api.nvim_command("wincmd " .. direction)
+	else
+		local command = "kitten @ action neighboring_window " .. mappings[direction]
+		vim.fn.system(command)
+	end
+end
+
+function M.navigateLeft()
+	M.navigate("h")
+end
+
+function M.navigateRight()
+	M.navigate("l")
+end
+function M.navigateUp()
+	M.navigate("k")
+end
+function M.navigateDown()
+	M.navigate("j")
+end
+
+---@param options Options
+function M.setup(options)
+	vim.keymap.set("n", options.keybindings.left or "<D-h>", M.navigateLeft, { silent = true })
+	vim.keymap.set("n", options.keybindings.right or "<D-l>", M.navigateRight, { silent = true })
+	vim.keymap.set("n", options.keybindings.up or "<D-k>", M.navigateUp, { silent = true })
+	vim.keymap.set("n", options.keybindings.down or "<D-j>", M.navigateDown, { silent = true })
+end
+
+vim.fn.system("kitten @ set-user-vars ISNVIM=true")
+
+return M
